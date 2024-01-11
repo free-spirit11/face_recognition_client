1724bdb38c570c9654ef9a65c623058ef719fe6b
Kovalchuk Serhii <sergii.kov@getblend.com>
2024-01-10 20:04:52 -0500

    added aws lambda function for ranking

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
+
+    const [emoji, setEmoji] = useState('');
+
+    const generateEmoji = (entries) => {
+        fetch(`https://6aekm53adf.execute-api.us-east-1.amazonaws.com/rank?rank=${entries}`)
+            .then(response => response.json())
+            .then(data => {
+                setEmoji(data.input)
+            })
+            .catch(console.log);
+    }
+
+    useEffect(() => {
+        generateEmoji(entries);
+    }, [entries])
+
     return (
         <div>
             <div className='white f3'>
@@ -8,6 +25,9 @@ const Rank = ({ name, entries }) => {
             </div>
             <div className='white f1'>
                 {entries}
+            </div>
+            <div className='white f1'>
+                {`Rank Badge: ${emoji}`}
             </div>
         </div>
     );
